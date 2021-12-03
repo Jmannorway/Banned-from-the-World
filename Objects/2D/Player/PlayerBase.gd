fb77f585fe298d3f55fbe0645eb82dfaa40a6bca
Jmannorway <jarklu@protonmail.com>
2021-12-03 16:42:54 +0100

    begonus slongus
    I removed things because it helps to do something simple in order to get back into the grove

--- Objects/2D/Player/PlayerBase.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-class_name PlayerBase
-
-var speed : float = 100.0
-var run_speed : float = 175.0
-var grid : Vector3 = Vector3.ONE * 16.0
-var offset : Vector3 = grid / 2
-var direction : Vector3 = Vector3.ZERO
