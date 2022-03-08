05da8ca990361674d72892964f520892d043ed52
Mads Gjengaar <mig.gjengaar@gmail.com>
2022-03-08 17:46:05 +0100

    Floor Handler and parts of the stairs

---
+++ scripts/Room2D/FloorHandler2D.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+export var floorsPath: NodePath
+export var stairsPath: NodePath
+
+var currentFloor: int setget set_floor
+
+onready var floors: Array = get_node(floorsPath).get_children()
+onready var staircases: Array = get_node(stairsPath).get_children()
+
+signal changed_floor(floorIndex)
+
+func _ready():
+	for stair in staircases:
+		stair.connect("change_floors", self, "set_relative_floor")
+	
+	set_floor(0)
+
+func set_floor(var index: int) -> void:
+	currentFloor = index
+	
+	print(currentFloor)
+	
+	floors[currentFloor].get_node("solid_grid_2d").paint_to_world_grid()
+	
+	emit_signal("changed_floor", currentFloor)
+
+func set_relative_floor(var elevation: int) -> void:
+	set_floor(currentFloor + elevation)
