32aaf43173d1c98ec8d802800c4701264793b7b8
Jmannorway <54485431+Jmannorway@users.noreply.github.com>
2022-05-01 23:27:16 +0200

    Small fixes after klaudia import
    - Readded wall behind Max
    - Heightened bed

--- scripts/Game/GameControl.gd
+++ scripts/Game/GameControl.gd
@@ -1,25 +1,22 @@
 extends AnimationPlayer
 
 export(bool) var debug
-export(String) var starting_animation
-export(bool) var start_on_ready
-export(String) var door_cutscene_name
+export(String) var debug_animation_name
+
+export(String) var opening_animation_name
+export(String) var door_animation_name
 
 func _ready():
 	var _room_manager = Util.get_first_node_in_group(get_tree(), "room_manager") as RoomManager3D
 	if debug:
 		_room_manager.controllingCamera = false
-		if start_on_ready:
-			print(44)
-			call_deferred("play", starting_animation)
+		call_deferred("play", debug_animation_name)
 	else:
 		if Statistics.metadata.intro_finished:
 			_room_manager.controllingCamera = true
 		else:
 			_room_manager.controllingCamera = false
-			call_deferred("play", "opening_cutscene")
-#			play("opening_cutscene")
-			print(42)
+			call_deferred("play", opening_animation_name)
 	
 	var _tutorial = get_node("../cutscene_nodes/tutorial")
 	_tutorial.connect(
@@ -29,11 +26,11 @@ func _ready():
 			[_tutorial])
 
 func _on_door_interactable_interacted() -> void:
-	play(door_cutscene_name)
+	play(door_animation_name)
 
 func _on_cutscene_player_animation_finished(anim_name: String) -> void:
 	pass
-	if anim_name == door_cutscene_name:
+	if anim_name == door_animation_name:
 		Statistics.metadata.intro_finished = true
 		Statistics.write_meta_file()
 
