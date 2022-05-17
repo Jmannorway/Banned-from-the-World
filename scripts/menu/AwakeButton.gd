29cee59f1550cfddbebf5771149deb128978b2b6
Mads Gjengaar <mig.gjengaar@gmail.com>
2022-05-17 20:39:52 +0200

    menu fix lol hahahahahahahahahahah

--- scripts/menu/AwakeButton.gd
+++ scripts/menu/AwakeButton.gd
@@ -3,4 +3,7 @@ extends ButtonAction
 export (int, "Outer", "Inner")var sendToWorld
 
 func send_action() -> void:
+	# make sure we unpause... cause that's annoying when it doesn't
+	emit_signal("action", 0, "resume")
+	
 	Temp.goto_world(get_tree(), sendToWorld)
